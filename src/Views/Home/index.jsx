53047cd9a8c39b2764653ccd54cc4abed687026f
Yen Ka <yenkaphu96pnv@gmail.com>
2021-08-31 16:28:20 +0700

    [E-Learning]: Implement top categories section

--- src/Views/Home/index.jsx
+++ src/Views/Home/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../Components/Header";
 import Banner from "../../Components/Banner";
 import Footer from "../../Components/Footer";
+import TopCateGories from "../../Components/TopCategories";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Banner />
+      <TopCateGories />
       <Footer />
     </>
   );
