f318597112cdcb8463a32c4e2069ba21ef35dcfa
Yen Ka <yenkaphu96pnv@gmail.com>
2021-09-09 16:56:50 +0700

    [E-Learning]: Implement register course and reverse course

--- src/Services/baseService.js
+++ src/Services/baseService.js
@@ -1,24 +1,28 @@
 import Axios from "axios";
 import { DOMAIN, TOKEN } from "../util/settings/config";
+
 export class baseService {
-    get = (url) => {
-        return Axios({
-            url: `${DOMAIN}/${url}`,
-            method: "GET",
-            headers: {
-                TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNjQiLCJIZXRIYW5TdHJpbmciOiIyMS8wMS8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NDI3MjMyMDAwMDAiLCJuYmYiOjE2MTYxNzMyMDAsImV4cCI6MTY0Mjg3MDgwMH0.2sSWVGy-3Ce9iJ8bIYmYOJ9aE1eu3fz07DtA2ECfiyk",
-            },
-        });
-    };
-    post = (url, model) => {
-        return Axios({
-            url: `${DOMAIN}/${url}`,
-            method: "POST",
-            data: model,
-            headers: {
-                Authorization: "Bearer " + localStorage.getItem(TOKEN),
-                TokenCybersoft: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNjQiLCJIZXRIYW5TdHJpbmciOiIyMS8wMS8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NDI3MjMyMDAwMDAiLCJuYmYiOjE2MTYxNzMyMDAsImV4cCI6MTY0Mjg3MDgwMH0.2sSWVGy-3Ce9iJ8bIYmYOJ9aE1eu3fz07DtA2ECfiyk",
-            },
-        });
-    };
+  get = (url) => {
+    return Axios({
+      url: `${DOMAIN}/${url}`,
+      method: "GET",
+      headers: {
+        TokenCybersoft:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNjQiLCJIZXRIYW5TdHJpbmciOiIyMS8wMS8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NDI3MjMyMDAwMDAiLCJuYmYiOjE2MTYxNzMyMDAsImV4cCI6MTY0Mjg3MDgwMH0.2sSWVGy-3Ce9iJ8bIYmYOJ9aE1eu3fz07DtA2ECfiyk",
+      },
+    });
+  };
+  
+  post = (url, model) => {
+    return Axios({
+      url: `${DOMAIN}/${url}`,
+      method: "POST",
+      data: model,
+      headers: {
+        Authorization: "Bearer " + localStorage.getItem(TOKEN),
+        TokenCybersoft:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJGcm9udCBFbmQgNjQiLCJIZXRIYW5TdHJpbmciOiIyMS8wMS8yMDIyIiwiSGV0SGFuVGltZSI6IjE2NDI3MjMyMDAwMDAiLCJuYmYiOjE2MTYxNzMyMDAsImV4cCI6MTY0Mjg3MDgwMH0.2sSWVGy-3Ce9iJ8bIYmYOJ9aE1eu3fz07DtA2ECfiyk",
+      },
+    });
+  };
 }
